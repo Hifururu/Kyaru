2aa075b68f9f9cf3bf7c317da030621bd33433fe
Hifururu <felipeeduardoa@gmail.com>
2025-10-06 15:40:37 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  // opcional, pero útil en PaaS:
-  experimental: { outputFileTracingRoot: __dirname },
-};
-module.exports = nextConfig;
+  reactStrictMode: true,
+  // Si no usas alias, puedes borrar todo el bloque webpack.
+  webpack: (config) => {
+    // ejemplo de alias raíz "@"
+    config.resolve.alias['@'] = path.resolve(__dirname)
+    return config
+  },
+}
+
+export default nextConfig
