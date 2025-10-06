b260acf2a38c30b044105c6b9322b4028796b74e
Hifururu <felipeeduardoa@gmail.com>
2025-10-06 18:09:27 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 🔒 Evita que Next intente usar TypeScript en el build
-  typescript: { ignoreBuildErrors: true },
-  // (opcional, evita que ESLint tumbe el build)
-  eslint: { ignoreDuringBuilds: true },
+  eslint: { ignoreDuringBuilds: true }
 };
-
 module.exports = nextConfig;
